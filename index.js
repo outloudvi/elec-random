3b826995c23786bccd5e1cfa948c0fc049483dfd
Outvi V <olvi.git@mailnull.com>
2018-12-02 19:47:41 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, session } = require('electron');
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 600,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+  win.loadFile('index.html');
+  // win.webContents.openDevTools();
+  win.on('closed', () => {
+    win = null
+  });
+
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (win == null) {
+    createWindow();
+  }
+})
